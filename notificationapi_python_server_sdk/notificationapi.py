625cd03c8d45fab07d87e417b25b72152b67f78c
Sahand Seifi <sahand.seifi@gmail.com>
2021-04-08 14:08:28 -0230

    Create init and send functions

--- notificationapi_python_server_sdk/notificationapi.py
+++ notificationapi_python_server_sdk/notificationapi.py
@@ -1,2 +1,37 @@
-def init():
-    return "Initialized"
+import requests
+import logging
+
+__client_id = ""
+__client_secret = ""
+
+
+def init(client_id, client_secret):
+    if not client_id:
+        raise Exception("Bad client_id")
+
+    if not client_secret:
+        raise Exception("Bad client_secret")
+
+    global __client_id
+    __client_id = client_id
+    global __client_secret
+    __client_secret = client_secret
+
+
+def send(params):
+    api_url = "https://api.notificationapi.com/" + __client_id + "/sender"
+    response = requests.post(
+        api_url,
+        auth=requests.auth.HTTPBasicAuth(
+            username=__client_id, password=__client_secret
+        ),
+        json=params,
+    )
+    if response.status_code == 202:
+        logging.warning("NotificationAPI request ignored. params: %s", params)
+    if response.status_code == 500:
+        logging.error(
+            "NotificationAPI request failed. Response: %s. params: %s",
+            response.text,
+            params,
+        )
--- tests/test_notificationapi.py
+++ tests/test_notificationapi.py
@@ -2,8 +2,127 @@
 
 """Tests for `notificationapi_python_server_sdk` package."""
 
-from notificationapi_python_server_sdk import notificationapi
+import pytest
+from notificationapi_python_server_sdk import (
+    notificationapi,
+)
+
+client_id = "client_id"
+client_secret = "client_secret"
+user = {
+    "id": "userId",
+    "email": "test+python_server_sdk@notificationapi.com",
+}
+notification_id = "notification_id"
+send_api_path = f"https://api.notificationapi.com/{client_id}/sender"
 
 
-def test_init():
-    assert notificationapi.init() == "Initialized"
+def test_init_raises_given_empty_client_id():
+    with pytest.raises(Exception, match="Bad client_id"):
+        notificationapi.init("", "client_secret")
+
+
+def test_init_raises_given_empty_client_secret():
+    with pytest.raises(Exception, match="Bad client_secret"):
+        notificationapi.init("client_id", "")
+
+
+def test_init_passes_given_id_and_secret():
+    notificationapi.init(client_id, client_secret)
+
+
+def test_send_makes_one_post_api_call(requests_mock):
+    requests_mock.post(send_api_path)
+    notificationapi.init(client_id, client_secret)
+    notificationapi.send({"user": user, "notificationId": notification_id})
+    assert requests_mock.call_count == 1
+
+
+def test_send_uses_basic_authorization(requests_mock):
+    requests_mock.post(send_api_path)
+    notificationapi.init(client_id, client_secret)
+    notificationapi.send({"user": user, "notificationId": notification_id})
+    assert (
+        requests_mock.last_request.headers["Authorization"]
+        == "Basic Y2xpZW50X2lkOmNsaWVudF9zZWNyZXQ="
+    )
+
+
+def test_send_includes_notification_id_and_user_in_request_body(
+    requests_mock,
+):
+    requests_mock.post(send_api_path)
+    notificationapi.init(client_id, client_secret)
+    notificationapi.send({"user": user, "notificationId": notification_id})
+    assert requests_mock.last_request.json() == {
+        "notificationId": notification_id,
+        "user": user,
+    }
+
+
+def test_send_includes_merge_tags_in_request_body(
+    requests_mock,
+):
+    requests_mock.post(send_api_path)
+    notificationapi.init(client_id, client_secret)
+    notificationapi.send(
+        {
+            "user": user,
+            "notificationId": notification_id,
+            "mergeTags": {"x": "y"},
+        }
+    )
+    assert requests_mock.last_request.json() == {
+        "notificationId": notification_id,
+        "user": user,
+        "mergeTags": {"x": "y"},
+    }
+
+
+def test_send_includes_email_options_in_request_body(
+    requests_mock,
+):
+    requests_mock.post(send_api_path)
+    notificationapi.init(client_id, client_secret)
+    email_options = {
+        "email": {
+            "bccAddresses": ["test@test.com"],
+            "ccAddresses": ["test@test.com"],
+            "replyToAddresses": ["test@test.com"],
+        }
+    }
+    notificationapi.send(
+        {
+            "user": user,
+            "notificationId": notification_id,
+            "emailOptions": email_options,
+        }
+    )
+    assert requests_mock.last_request.json() == {
+        "notificationId": notification_id,
+        "user": user,
+        "emailOptions": email_options,
+    }
+
+
+def test_send_logs_if_it_gets_202(requests_mock, caplog):
+    requests_mock.post(send_api_path, status_code=202)
+    notificationapi.init(client_id, client_secret)
+    notificationapi.send({"user": user, "notificationId": notification_id})
+    assert "NotificationAPI request ignored." in caplog.text
+
+
+def test_send_logs_on_202(requests_mock, caplog):
+    requests_mock.post(send_api_path, status_code=202)
+    notificationapi.init(client_id, client_secret)
+    notificationapi.send({"user": user, "notificationId": notification_id})
+    assert "NotificationAPI request ignored." in caplog.text
+
+
+def test_send_logs_and_throws_on_500(requests_mock, caplog):
+    requests_mock.post(send_api_path, status_code=500, text="big oof 500")
+    notificationapi.init(client_id, client_secret)
+    notificationapi.send({"user": user, "notificationId": notification_id})
+    assert (
+        "NotificationAPI request failed. Response: big oof 500" in caplog.text
+    )
