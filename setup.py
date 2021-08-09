01f6ac3682b4baebbaed67716b892f1174f628b7
Sahand Seifi <sahand.seifi@gmail.com>
2021-08-09 16:31:06 -0230

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/notificationapi-com/notificationapi_python_server_sdk",
-    version="0.0.5",
+    version="0.0.6",
     zip_safe=False,
 )
