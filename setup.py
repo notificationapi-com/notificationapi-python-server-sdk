b1bbd0aa408704045fe4d5fd8881c51aa34dbeb9
mbasadi <asadi.mohammadbagher@gmail.com>
2022-01-05 16:23:58 -0330

    Bump version: 0.0.7 → 0.1.7

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/notificationapi-com/notificationapi_python_server_sdk",
-    version="0.0.8",
+    version="0.1.7",
     zip_safe=False,
 )
