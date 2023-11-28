2072ce4e74de5694c17897d4b520b2f43f13399f
mbasadi <asadi.mohammadbagher@gmail.com>
2023-11-27 19:46:57 -0500

    Refactor package to use async HTTP requests with HTTPX

--- notificationapi_python_server_sdk/notificationapi.py
+++ notificationapi_python_server_sdk/notificationapi.py
@@ -1,8 +1,9 @@
-import requests
+import httpx
 import logging
 import hashlib
 import base64
 import urllib.parse
+
 __client_id = ""
 __client_secret = ""
 
@@ -20,7 +21,7 @@ def init(client_id, client_secret):
     __client_secret = client_secret
 
 
-def request(method, uri, data=None, custom_auth=None):
+async def request(method, uri, data=None, custom_auth=None):
     api_url = "https://api.notificationapi.com/" + __client_id + "/" + uri
 
     headers = {}
@@ -29,12 +30,13 @@ def request(method, uri, data=None, custom_auth=None):
     else:
         headers['Authorization'] = 'Basic ' + base64.b64encode(f'{__client_id}:{__client_secret}'.encode()).decode()
 
-    response = requests.request(
-        method,
-        api_url,
-        headers=headers,
-        json=data,
-    )
+    async with httpx.AsyncClient() as client:
+        response = await client.request(
+            method,
+            api_url,
+            headers=headers,
+            json=data,
+        )
     if response.status_code == 202:
         logging.warning("NotificationAPI request ignored. params: %s", data)
     if response.status_code == 500:
@@ -45,16 +47,16 @@ def request(method, uri, data=None, custom_auth=None):
         )
 
 
-def send(params):
-    request("POST", "sender", params)
+async def send(params):
+    await request("POST", "sender", params)
 
 
-def retract(params):
-    request("POST", "sender/retract", params)
+async def retract(params):
+    await request("POST", "sender/retract", params)
 
 
-def create_sub_notification(params):
-    request(
+async def create_sub_notification(params):
+    await request(
         "PUT",
         "notifications/%s/subNotifications/%s"
         % (params["notification_id"], params["sub_notification_id"]),
@@ -62,23 +64,23 @@ def create_sub_notification(params):
     )
 
 
-def delete_sub_notification(params):
-    request(
+async def delete_sub_notification(params):
+    await request(
         "DELETE",
         "notifications/%s/subNotifications/%s"
         % (params["notification_id"], params["sub_notification_id"]),
     )
 
 
-def set_user_preferences(params):
-    request(
+async def set_user_preferences(params):
+    await request(
         "POST",
         "user_preferences/%s" % (params["userId"]),
         params["userPreferences"],
     )
 
 
-def identify_user(params):
+async def identify_user(params):
     user_id = params.pop('id')
 
     hashed_user_id = hashlib.sha256((__client_secret + user_id).encode()).digest()
@@ -86,4 +88,4 @@ def identify_user(params):
 
     custom_auth = 'Basic ' + base64.b64encode(f'{__client_id}:{user_id}:{hashed_user_id_base64}'.encode()).decode()
 
-    request('POST', f'users/{urllib.parse.quote(user_id)}', params, custom_auth)
+    await request('POST', f'users/{urllib.parse.quote(user_id)}', params, custom_auth)
