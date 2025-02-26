baed448ef97961d901bf54b94a24abe45d10c7d2
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2025-02-25 22:34:11 -0330

    Add region constants for NotificationAPI SDK
    - Define constants for US, EU, and CA regions
    - Provide easy access to predefined base URLs for different geographical regions

--- notificationapi_python_server_sdk/__init__.py
+++ notificationapi_python_server_sdk/__init__.py
@@ -3,3 +3,8 @@
 __author__ = """Sahand Seifi"""
 __email__ = "sahand.seifi@gmail.com"
 __version__ = "1.2.0"
+
+# Region constants
+US_REGION = "https://api.notificationapi.com"
+EU_REGION = "https://api.eu.notificationapi.com"
+CA_REGION = "https://api.ca.notificationapi.com"
